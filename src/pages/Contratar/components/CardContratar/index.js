ba8bdbf133cc7a8c8f085de88a9bd84d9d7769a2
André Miranda <andrelmmiranda@gmail.com>
2022-03-19 23:00:32 -0300

    modificação na propriedade class para className dos componentes CardContratar e ContainerContratar.

--- src/pages/Contratar/components/CardContratar/index.js
+++ src/pages/Contratar/components/CardContratar/index.js
@@ -1,12 +1,12 @@
 const CardContratar = ({imagem, titulo, texto}) => {
     return (
-        <div class="card-contratar row cor-secundaria d-flex justify-content-center my-5">
-            <div class="col-12 col-sm-12 col-md-6 col-lg-4 d-flex justify-content-center">
-                <img id="img-hexagono" class="img-contratar align-self-center" src={imagem} alt="Card image cap"/>
+        <div className="card-contratar row cor-secundaria d-flex justify-content-center my-5">
+            <div className="col-12 col-sm-12 col-md-6 col-lg-4 d-flex justify-content-center">
+                <img id="img-hexagono" className="img-contratar align-self-center" src={imagem} alt="Card image cap"/>
             </div>
-            <div class="card-body col-12 col-sm-12 col-md-6 col-lg-6">
-                <h5 class="texto-principal text-left mt-5 mx-4 mb-3">{titulo}</h5>
-                <p class="texto-terciario-contratar text-justify mx-4">{texto}</p>
+            <div className="card-body col-12 col-sm-12 col-md-6 col-lg-6">
+                <h5 className="texto-principal text-left mt-5 mx-4 mb-3">{titulo}</h5>
+                <p className="texto-terciario-contratar text-justify mx-4">{texto}</p>
             </div>
         </div>
     );
