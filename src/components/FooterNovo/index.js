2ae05886506e823301027fa6dd223544cd59d3f2
André Miranda <andrelmmiranda@gmail.com>
2022-03-19 22:51:27 -0300

    modificação de atributo class para className no componente footerNovo.

--- src/components/FooterNovo/index.js
+++ src/components/FooterNovo/index.js
@@ -5,12 +5,12 @@ import '../header_footer.css';
 function FooterNovo() {
     return(
         <footer id="myFooter">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-12 col-md-3 col-lg-3">
-                        <a><img src="./img/logo/logo-completa.png" class="logo-footer mt-4" alt=""/></a>
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-12 col-md-3 col-lg-3">
+                        <a><img src="./img/logo/logo-completa.png" className="logo-footer mt-4" alt=""/></a>
                     </div>
-                    <div class="col-sm-12 col-md-2 col-lg-2">
+                    <div className="col-sm-12 col-md-2 col-lg-2">
                         <h5>Links Rápidos</h5>
                         <ul>
                             <li><a href="http://localhost:3000/Home">Home</a></li>
@@ -18,7 +18,7 @@ function FooterNovo() {
                             <li><a href="http://localhost:3000/Podcasts">Podcasts</a></li>
                         </ul>
                     </div>
-                    <div class="col-sm-12 col-md-2 col-lg-2">
+                    <div className="col-sm-12 col-md-2 col-lg-2">
                         <h5>Links Rápidos</h5>
                         <ul>
                             <li><a href="http://localhost:3000/Contratar">Contratar</a></li>
@@ -26,25 +26,25 @@ function FooterNovo() {
                             <li><a href="http://localhost:3000/Cadastrar">Cadastre-se</a></li>
                         </ul>
                     </div>
-                    <div class="col-sm-12 col-md-2 col-lg-2">
+                    <div className="col-sm-12 col-md-2 col-lg-2">
                         <h5>Apoiador</h5>
                         <ul>
                             <li><a target={"_blanck"} href="https://recodepro.org.br/">Recode Pro</a></li>
                         </ul>
                     </div>
-                    <div class="col-sm-12 col-md-3 col-lg-3">
-                        <div class="social-networks">
-                            <a href="#" class="twitter"><i class="fa fa-github"></i></a>
-                            <a href="#" class="facebook"><i class="fa fa-linkedin"></i></a>
-                            <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
+                    <div className="col-sm-12 col-md-3 col-lg-3">
+                        <div className="social-networks">
+                            <a href="#" className="twitter"><i className="fa fa-github"></i></a>
+                            <a href="#" className="facebook"><i className="fa fa-linkedin"></i></a>
+                            <a href="#" className="instagram"><i className="fa fa-instagram"></i></a>
                         </div>
                         <a href="#">
-                            <button type="button" class="btn btn-default">Contato</button>
+                            <button type="button" className="btn btn-default">Contato</button>
                         </a>
                     </div>
                 </div>
         </div>
-        <div class="footer-copyright">
+        <div className="footer-copyright">
             <p>© 2022 Copyright - Black Code Brasil</p>
         </div>
     </footer>
